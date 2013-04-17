3c0cde5cc0177b632a98c1d69df895c5fc6be3c1
Kenny <seiji.kaldeara@gmail.com>
2013-04-17 11:37:03 +0200

    composer update

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitb4b8e31a5aebd45f1d58443bcfa52dac::getLoader();
+return ComposerAutoloaderInit673f5be73342306d714453c386dec875::getLoader();
