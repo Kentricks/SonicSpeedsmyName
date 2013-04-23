3434568ca4b8d1ec16c8afb203ad355306a85278
Kenny <seiji.kaldeara@gmail.com>
2013-04-23 17:18:27 +0200

    forgot pw page

--- src/Etna/SocialBundle/Controller/DefaultController.php
+++ src/Etna/SocialBundle/Controller/DefaultController.php
@@ -42,4 +42,26 @@ class DefaultController extends Controller
 		return $this->render('EtnaSocialBundle:Default:newcompte.html.twig', array('form' => $form->createView(),
 			));
     }
+
+    public function forgotAction()
+    {
+        $formBuilder = $this->createFormBuilder();
+        $formBuilder
+            ->add('email', 'email');
+        $form = $formBuilder->getForm();
+        $request = $this->get('request');
+
+        if ($request->getMethod() == 'POST') {
+            $form->bind($request);
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->flush();
+
+                return $this->redirect($this->generateUrl('etna_social_login'));
+            }
+        }
+        $form->bind($request);
+        return $this->render('EtnaSocialBundle:Default:forgot.html.twig', array('form' => $form->createView(),
+        ));
+    }
 }
