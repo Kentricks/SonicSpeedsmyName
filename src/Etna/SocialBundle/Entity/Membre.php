3963320aaf221748b2f4bd8d40752502452dcbae
unknown <daadams@LFR408574.corp.capgemini.com>
2013-04-26 12:13:50 +0200

    Ajustement due au modif de champs, Sonic.

--- src/Etna/SocialBundle/Entity/Membre.php
+++ src/Etna/SocialBundle/Entity/Membre.php
@@ -278,7 +278,7 @@ class Membre extends BaseUser
     public function __construct()
     {
         parent::__construct();
-        $this->date_inscription = new \DateTime();
+        $this->dateInscription = new \DateTime();
         $this->friendsWithMe = new \Doctrine\Common\Collections\ArrayCollection();
         $this->myFriends = new \Doctrine\Common\Collections\ArrayCollection();
         $this->statuts = new \Doctrine\Common\Collections\ArrayCollection();
